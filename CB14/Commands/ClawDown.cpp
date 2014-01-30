dc3aca8959735a2a8e7f2305de73f60e66b97e8c
Alex Werner <6werner@arrowheadschools.org>
2014-01-30 17:38:13 -0600

    Danny Commands

--- CB14/Commands/ClawDown.cpp
+++ CB14/Commands/ClawDown.cpp
@@ -12,8 +12,6 @@
 #include "ClawDown.h"
 #include "../Subsystems/Claw.h"
 
-bool low = false;
-
 ClawDown::ClawDown() {
 	// Use requires() here to declare subsystem dependencies
 	// eg. requires(chassis);
@@ -28,15 +26,7 @@ void ClawDown::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void ClawDown::Execute() {
-	if(!low) { //if claw is not too low
-		RobotMap::clawlifter->Set(-.2); //move down
-	}
-	else { //if it is
-		RobotMap::clawlifter->Set(0); //stop
-	}
-	if(Robot::claw->pot1->GetValue() <= 0) { //if the claw is at bottom
-		low = true; //claw is too low
-	}
+	RobotMap::clawlifter->Set(-.2);
 }
 
 // Make this return true when this Command no longer needs to run execute()
--- CB14/Commands/ClawMid.cpp
+++ CB14/Commands/ClawMid.cpp
@@ -12,7 +12,7 @@
 #include "ClawMid.h"
 #include "../Subsystems/Claw.h"
 
-float mid = 10; //set mid position
+float mid = 10;
 
 ClawMid::ClawMid() {
 	// Use requires() here to declare subsystem dependencies
@@ -23,23 +23,23 @@ ClawMid::ClawMid() {
 
 // Called just before this Command runs the first time
 void ClawMid::Initialize() {
-	Robot::claw->pot1->GetValue(); //get pot value
+	Robot::claw->pot1->GetValue();
 }
 
 // Called repeatedly when this Command is scheduled to run
 void ClawMid::Execute() {
-	float midat = Robot::claw->pot1->GetValue(); //assign value to a variable to avoid writing everything
-	float midspd = 1-midat/mid; //determine speed based on position relative to the mid setpoint
-	if(midspd >.7){ //if the speed is over .7
-		midspd = .7; //keep it at .7
+	float midat = Robot::claw->pot1->GetValue();
+	float midspd = 1-midat/mid;
+	if(midspd >.7){
+		midspd = .7;
 	}
-	if (midspd <.1 && midspd !=0) { //if the speed is under .1 but not 0
-		midspd = .1; //keep it at .1
+	if (midspd <.1 && midspd !=0) {
+		midspd = .1;
 	}
-	if(midat>10) { //if the position is over 10
-		midspd = 0; //stop
+	if(midat>10) {
+		midspd = 0;
 	}
-	RobotMap::clawlifter->Set(midspd); //set the speed of the claw to value determined above
+	RobotMap::clawlifter->Set(midspd);
 }
 
 // Make this return true when this Command no longer needs to run execute()
